5c66e024e29ad33209943fd7bcf633495f5bc24e
felipe gomez <ggfelipegc@gmail.com>
2023-08-10 14:48:01 -0500

    fix: fixup linter issues

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,9 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-	res.send('Hello, Proyecto Beta!');
+  res.send('Hello, Proyecto Beta!');
 });
 
 app.listen(port, () => {
-	console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
